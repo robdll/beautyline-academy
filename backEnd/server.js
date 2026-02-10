a47cd17e8d62c214b52789ce860d0b94e6039b17
igorrm23 <igormachadofire14@gmail.com>
2026-02-09 22:20:10 -0300

    add dontenv

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
+require("dotenv").config();
+
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json());
