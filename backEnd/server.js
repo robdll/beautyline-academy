2f9492b34fc9bdf2546fb4e91d6ad4f95509a0ff
Guilherme Pereira Lima <105331166+Guibis@users.noreply.github.com>
2026-02-11 16:44:22 -0300

    Update BackEnd (#2)
    * recreating back end
    
    * new line and .gitignore
    
    ---------
    
    Co-authored-by: igorrm23 <igormachadofire14@gmail.com>

---
+++ backEnd/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors())
+app.use(express.json());
+
+app.get("/teste", (req, res) => {
+    res.json({ msg: "Hello word" })
+})
+
+app.listen(PORT, () => {
+    console.log("Server running in port ", PORT)
+})
+
