86937168875562d58e136e405e94a028cb31b203
igorrm23 <igormachadofire14@gmail.com>
2026-02-11 23:21:20 -0300

    Initial database configurations

--- backEnd/server.js
+++ backEnd/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const Router = require("./routes/user.router");
 const morgan = require("morgan");
 const connectDB = require("./config/DBmongo");
+const User = require("./model/user.model");
 require("dotenv").config();
 
 connectDB();
@@ -15,6 +16,17 @@ app.use(express.json());
 app.use(morgan("common"));
 app.use("/", Router);
 
+
+app.post('/usuarios', async (req, res) => {
+    try {
+
+      const novoUsuario = await User.create(req.body);
+      res.status(201).json(novoUsuario);
+
+    } catch (error) {
+      res.status(400).json({ erro: error.message });
+    }
+});
 
 
 
