431a44aea84beb652a41dd916295079a749c3a6b
igorrm23 <igormachadofire14@gmail.com>
2026-02-24 12:23:54 -0300

    file DBmongp/corretion of error

--- backEnd/config/DBmongo.js
+++ backEnd/config/DBmongo.js
@@ -3,13 +3,12 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
 
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
-
-        });
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
 
     } catch (error) {
+<<<<<<< Updated upstream
         if (error.name === 'ValidationError') {
             console.error(`Validation Error: ${error.message}`);
             if (process.env.NODE_ENV !== 'test') process.exit(1);
@@ -17,6 +16,10 @@ const connectDB = async () => {
         }
 
         if (error.name === 'MongooseError' || (mongoose.Error && error instanceof mongoose.Error)) {
+=======
+
+        if (mongoose?.Error?.MongooseError && error instanceof mongoose.Error.MongooseError) {
+>>>>>>> Stashed changes
             console.error(`Mongoose Error: ${error.message}`);
             if (process.env.NODE_ENV !== 'test') process.exit(1);
             throw error;
