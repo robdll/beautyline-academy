ef0bddc47579cd944587cc31694714c64a4a85b0
Igor Rodrigues Machado <107290125+igorrm19@users.noreply.github.com>
2026-02-27 00:27:13 -0300

    Clean up whitespace in equipament.model.js
    Removed unnecessary whitespace in equipment model schema.

--- backEnd/model/equipament.model.js
+++ backEnd/model/equipament.model.js
@@ -2,35 +2,25 @@
 const mongoose = require("mongoose");
 
 const equipmentSchema = new mongoose.Schema({
-
     name: {
         type: String,
         required: true,
         unique: true
     },
-
     description: String,
-
     category: String,
-
     salePrice: Number,
     rentalPrice: Number,
-
     availableForSale: {
         type: Boolean,
         default: true
     },
-
     availableForRental: {
         type: Boolean,
         default: true
     },
-
     stock: Number,
-
     image: String
-
 }, { timestamps: true });
-
 module.exports = mongoose.model("Equipment", equipmentSchema);
 
