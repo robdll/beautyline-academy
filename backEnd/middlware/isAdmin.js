3f514428d01533f2c7b07aff7ed76191c0c2717b
igorrm23 <igormachadofire14@gmail.com>
2026-03-02 22:17:04 -0300

    req.user as unauthorized

--- backEnd/middlware/isAdmin.js
+++ backEnd/middlware/isAdmin.js
@@ -1,6 +1,6 @@
 const isAdmin = (req, res, next) => {
-    if (req.user.role !== "admin") {
-        return res.status(403).json({ message: "You are not authorized to perform this action" });
+    if (!req.user || req.user.role !== "admin") {
+        return res.status(403).json({ message: "Access denied" });
     }
     next();
 };
