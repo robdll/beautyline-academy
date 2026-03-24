c9fc3128cbdc88fd21108f5e4c40e7b40bd5a9aa
igorrm23 <igormachadofire14@gmail.com>
2026-03-24 00:38:59 -0300

    feat: enhance logging with detailed object data for successful operations and include error messages in API responses and error logs.

--- backEnd/controlrs/product.controlrs.js
+++ backEnd/controlrs/product.controlrs.js
@@ -10,33 +10,57 @@ const getProducts = async (req, res) => {
         const products = await Product.find();
         if (products.length === 0) {
             return res.status(200).json([]);
-            logger.info(SUCCESS_MESSAGES.PRODUCT_FOUND);
+            logger.info(SUCCESS_MESSAGES.PRODUCT_FOUND, {
+                id: products._id,
+                name: products.name,
+                price: products.price,
+                category: products.category,
+                tags: products.tags,
+                stock: products.stock,
+                brand: products.brand
+            });
         }
         res.status(200).json(products);
-        logger.info(SUCCESS_MESSAGES.PRODUCT_FOUND);
+        logger.info(SUCCESS_MESSAGES.PRODUCT_FOUND, {
+            id: products._id,
+            name: products.name,
+            price: products.price,
+            category: products.category,
+            tags: products.tags,
+            stock: products.stock,
+            brand: products.brand
+        });
 
     } catch (err) {
         logger.error(err);
-        res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+        res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR, error: err.message });
     }
 }
 const getProductById = async (req, res) => {
     try {
         const product = await Product.findById(req.params.id);
         if (!product) {
-            return res.status(404).send({ message: ERROR_MESSAGES.PRODUCT_NOT_FOUND });
-            logger.error(ERROR_MESSAGES.PRODUCT_NOT_FOUND);
+            return res.status(404).send({ message: ERROR_MESSAGES.PRODUCT_NOT_FOUND, error: err.message });
+            logger.error(ERROR_MESSAGES.PRODUCT_NOT_FOUND, { error: err.message });
         }
         res.status(200).json(product);
-        logger.info(SUCCESS_MESSAGES.PRODUCT_FOUND);
+        logger.info(SUCCESS_MESSAGES.PRODUCT_FOUND, {
+            id: product._id,
+            name: product.name,
+            price: product.price,
+            category: product.category,
+            tags: product.tags,
+            stock: product.stock,
+            brand: product.brand
+        });
 
     } catch (err) {
         if (err.name === "CastError") {
-            return res.status(400).json({ message: ERROR_MESSAGES.INVALID_ID });
-            logger.error(ERROR_MESSAGES.INVALID_ID);
+            return res.status(400).json({ message: ERROR_MESSAGES.INVALID_ID, error: err.message });
+            logger.error(ERROR_MESSAGES.INVALID_ID, { error: err.message });
         }
         logger.error(err);
-        res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+        res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR, error: err.message });
     }
 }
 const createProduct = async (req, res) => {
@@ -66,7 +90,15 @@ const createProduct = async (req, res) => {
             image
         });
         const savedProduct = await newProduct.save();
-        logger.info(SUCCESS_MESSAGES.PRODUCT_CREATED);
+        logger.info(SUCCESS_MESSAGES.PRODUCT_CREATED, {
+            id: savedProduct._id,
+            name: savedProduct.name,
+            price: savedProduct.price,
+            category: savedProduct.category,
+            tags: savedProduct.tags,
+            stock: savedProduct.stock,
+            brand: savedProduct.brand
+        });
         res.status(201).json(savedProduct);
 
     } catch (err) {
@@ -75,16 +107,16 @@ const createProduct = async (req, res) => {
                 message: ERROR_MESSAGES.INVALID_PRODUCT_DATA,
                 details: err.errors
             });
-            logger.error(ERROR_MESSAGES.INVALID_PRODUCT_DATA);
+            logger.error(ERROR_MESSAGES.INVALID_PRODUCT_DATA, { error: err.message });
         }
         if (err.code === DUPLICATED_PRODUCT_CODE) {
             return res.status(409).json({
                 message: ERROR_MESSAGES.DUPLICATED_PRODUCT
             });
-            logger.error(ERROR_MESSAGES.DUPLICATED_PRODUCT);
+            logger.error(ERROR_MESSAGES.DUPLICATED_PRODUCT, { error: err.message });
         }
         logger.error(err);
-        return res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+        return res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR, error: err.message });
     }
 }
 const updateProduct = async (req, res) => {
@@ -99,9 +131,18 @@ const updateProduct = async (req, res) => {
             }
         );
         if (!result) {
-            return res.status(404).send({ message: ERROR_MESSAGES.PRODUCT_NOT_FOUND });
+            return res.status(404).send({ message: ERROR_MESSAGES.PRODUCT_NOT_FOUND, error: err.message });
+            logger.error(ERROR_MESSAGES.PRODUCT_NOT_FOUND, { error: err.message });
         }
-        logger.info(SUCCESS_MESSAGES.PRODUCT_UPDATED);
+        logger.info(SUCCESS_MESSAGES.PRODUCT_UPDATED, {
+            id: result._id,
+            name: result.name,
+            price: result.price,
+            category: result.category,
+            tags: result.tags,
+            stock: result.stock,
+            brand: result.brand
+        });
         res.status(200).json(result);
 
     } catch (err) {
@@ -110,38 +151,47 @@ const updateProduct = async (req, res) => {
                 message: ERROR_MESSAGES.INVALID_PRODUCT_DATA,
                 details: err.errors
             });
-            logger.error(ERROR_MESSAGES.INVALID_PRODUCT_DATA);
+            logger.error(ERROR_MESSAGES.INVALID_PRODUCT_DATA, { error: err.message });
         }
         if (err.name === "CastError") {
-            return res.status(400).json({ message: ERROR_MESSAGES.INVALID_ID });
-            logger.error(ERROR_MESSAGES.INVALID_ID);
+            return res.status(400).json({ message: ERROR_MESSAGES.INVALID_ID, error: err.message });
+            logger.error(ERROR_MESSAGES.INVALID_ID, { error: err.message });
         }
         if (err.code === DUPLICATED_PRODUCT_CODE) {
             return res.status(409).json({
                 message: ERROR_MESSAGES.DUPLICATED_PRODUCT
             });
-            logger.error(ERROR_MESSAGES.DUPLICATED_PRODUCT);
+            logger.error(ERROR_MESSAGES.DUPLICATED_PRODUCT, { error: err.message });
         }
         logger.error(err);
-        return res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+        return res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR, error: err.message });
     }
 }
 const deleteProduct = async (req, res) => {
     try {
         const result = await Product.findByIdAndDelete(req.params.id);
         if (!result) {
-            return res.status(404).send({ message: ERROR_MESSAGES.PRODUCT_NOT_FOUND });
+            return res.status(404).send({ message: ERROR_MESSAGES.PRODUCT_NOT_FOUND, error: err.message });
+            logger.error(ERROR_MESSAGES.PRODUCT_NOT_FOUND, { error: err.message });
         }
-        logger.info(SUCCESS_MESSAGES.PRODUCT_DELETED);
+        logger.info(SUCCESS_MESSAGES.PRODUCT_DELETED, {
+            id: result._id,
+            name: result.name,
+            price: result.price,
+            category: result.category,
+            tags: result.tags,
+            stock: result.stock,
+            brand: result.brand
+        });
         res.status(200).send(SUCCESS_MESSAGES.PRODUCT_DELETED);
 
     } catch (err) {
         if (err.name === "CastError") {
-            return res.status(400).json({ message: ERROR_MESSAGES.INVALID_ID });
-            logger.error(ERROR_MESSAGES.INVALID_ID);
+            return res.status(400).json({ message: ERROR_MESSAGES.INVALID_ID, error: err.message });
+            logger.error(ERROR_MESSAGES.INVALID_ID, { error: err.message });
         }
         logger.error(err);
-        res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+        res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR, error: err.message });
     }
 }
 module.exports = {
